c5bdcdddd6a6f5db1da1712119fc5659da9d1c68
MZI <macchenjl@foxmail.com>
2018-11-22 00:32:05 +0800

    Add Image Previewer for article

--- source/js/index.js
+++ source/js/index.js
@@ -71,7 +71,84 @@
     this.releaseLock();
   };
 
+  function ImagePreviewer() {
+    var container = document.getElementsByClassName('post-content').item(0);
+    this.images = Array.prototype.map.call(container.getElementsByTagName('img'), function (img) {
+      return img;
+    });
+    this.showIdx = null;
+    this.popup = null;
+    this.preImage = null;
+    this.previewTimer = null;
+    this.lock = false;
+  }
+
+  ImagePreviewer.prototype.initPopup = function () {
+    var self = this;
+    this.popup = document.createElement('div');
+    this.preImage = document.createElement('img');
+    this.preImage.classList.add('preview-image');
+    this.popup.classList.add('image-previewer', 'popup');
+    this.preImageContainer = document.createElement('div');
+    this.preImageContainer.classList.add('preview-image-container');
+    this.preImageContainer.append(this.preImage);
+    this.popup.append(this.preImageContainer);
+    var shouldClose = null;
+    this.popup.addEventListener('click', function () {
+      if (shouldClose === null) {
+        shouldClose = true;
+      }
+      if (shouldClose && !self.lock && self.showIdx !== null) {
+        self.hide(function () {
+          shouldClose = null;
+        });
+      } else {
+        shouldClose = null;
+      }
+    });
+    this.preImageContainer.addEventListener('click', function () {
+      shouldClose = false;
+    });
+    document.body.append(this.popup);
+  };
+
+  ImagePreviewer.prototype.hide = function (callback) {
+    var self = this;
+    self.lock = true;
+    self.popup.classList.remove('show');
+    document.body.style.overflow = '';
+    self.previewTimer = setTimeout(function () {
+      clearTimeout(self.previewTimer);
+      self.lock = false;
+      self.showIdx = null;
+      self.popup.style.display = 'none';
+      callback && callback();
+    }, 500);
+  };
+
+  ImagePreviewer.prototype.init = function () {
+    var self = this;
+    this.initPopup();
+    this.images.forEach(function (image, idx) {
+      image.addEventListener('click', function () {
+        if (self.showIdx !== null) {
+          return;
+        }
+        self.showIdx = idx;
+        self.preImage.src = image.src;
+        self.popup.style.display = 'flex';
+        this.previewTimer = setTimeout(function () {
+          clearTimeout(this.previewTimer);
+          document.body.style.overflow = 'hidden';
+          self.popup.classList.add('show');
+        });
+      });
+    });
+  };
+
   var toc = new TOC('toc');
+  var imgPreviewr = new ImagePreviewer();
 
   toc.init();
+  imgPreviewr.init();
 })();
