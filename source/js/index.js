c98ac554dc561a9afc2d969838efe6aa787d1d9c
MZI <macchenjl@foxmail.com>
2020-05-28 00:53:56 +0800

    feat: use cdnjs

--- source/js/index.js
+++ source/js/index.js
@@ -11,17 +11,19 @@
       }
     });
   });
-  $('#main img').each(function () {
-    var element = document.createElement('a');
-    $(element).attr('data-fancybox', 'images');
-    $(element).attr('href', $(this).attr('src'));
-    $(element).addClass('image-link');
-    $(this).wrap(element);
-  });
-  var imageOpts = $.extend(true, {}, $.fancybox.defaults, {
-    caption: function (instance, item) {
-      return $(this).next('figcaption').html();
-    }
-  });
-  $('[data-fancybox="images"]').fancybox(imageOpts);
+  if (window.$ && window.$.fancybox) {
+    $('#main img').each(function () {
+      var element = document.createElement('a');
+      $(element).attr('data-fancybox', 'images');
+      $(element).attr('href', $(this).attr('src'));
+      $(element).addClass('image-link');
+      $(this).wrap(element);
+    });
+    var imageOpts = $.extend(true, {}, $.fancybox.defaults, {
+      caption: function (instance, item) {
+        return $(this).next('figcaption').html();
+      }
+    });
+    $('[data-fancybox="images"]').fancybox(imageOpts);
+  }
 })();
